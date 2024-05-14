69a611feaf0b24c6ade952a6d4d64c98392d3823
Gamaliel Adun <adungamaliel@gmail.com>
2024-05-14 17:37:27 +0100

    add function to check winner and cleanup

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 const board = ((num) => {
-  // const positions = {};
-  // for (let i = 0; i < num; i++){
-  //   for (let j = 0; j < num; j++){
-  //     positions[`pos_${i}${j}`] = null;
-  //   }
-  // }
-
   const positions = {};
+
   for (let i = 0; i < num*num; i++){
     positions[`${String(i)}`] = null;
   }
+
   return { positions }
 })(3);
 
@@ -31,7 +26,6 @@ function Player() {
 
 function gamePlay() {
   const playerPositions = [];
-
   const player = Player();
   
   function takePlayerInput() {
@@ -45,19 +39,42 @@ function gamePlay() {
       } else {
         board.positions[newPosition] = 'O'
       }
+      player.switchPlayer();
     }
-    
-    player.switchPlayer();
   }
 
-  // function checkWinner() {
+  function checkWinner() {
+    const winningCombos = [
+      [0,4,8], [0,1,2], [0,3,6], [1,4,7],
+      [2,4,6], [2,5,8], [3,4,5], [6,7,8]
+    ];
 
-  // }
+    for (const combo of winningCombos) {
+      const [a, b, c] = combo;
+      if (board.positions[a] === board.positions[b] && board.positions[b] === board.positions[c] && board.positions[c] !== null) {
+        return [true, board.positions[a]];
+      }
+    }
+    return false
+  }
 
   while (playerPositions.length < 9) {
     console.log(`Round ${playerPositions.length + 1}`);
     takePlayerInput();
-    console.log(board.positions);
+
+    console.log(playerPositions);
+    // console.log(board.positions);
+
+    let winner = checkWinner();
+
+    if (winner[0] === true) {
+      console.log(`${winner[1]} wins`);
+      break;
+    }
+  }
+
+  if (playerPositions.length === 9 && checkWinner() === false) {
+    console.log("Draw")
   }
 }
 
